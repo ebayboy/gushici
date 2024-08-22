8e43f750b784f98d84300151115fce79fc3d6acf
fanpengfei1 <fanpengfei@jd.com>
2024-08-22 10:49:29 +0800

    fw

---
+++ 20240820/parse_all.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("all.txt")
+	if err != nil {
+		fmt.Println("文件读取失败！", err)
+		return
+	}
+
+	cs := make(map[string][]string)
+
+	lines := strings.Split(string(content), "\n")
+	for _, line := range lines {
+		fmt.Println(line)
+		cols := strings.Split(line, " - ")
+		if len(cols) != 2 {
+			continue
+		}
+		fmt.Printf("line:[%s] len:%d\n", line, len(cols))
+		_, exist := cs[cols[1]]
+		if !exist {
+			cs[cols[1]] = make([]string, 0)
+		}
+		cs[cols[1]] = append(cs[cols[1]], cols[0])
+	}
+
+	for k, v := range cs {
+		fmt.Printf("%d %s [%v]\n", len(v), k, v)
+	}
+}
